b1f4785f2c94e29e0995206bf7d198bb918c629d
mahiro <suematsu-mahiro863@g.ecc.u-tokyo.ac.jp>
2023-06-06 04:36:07 +0900

    Authorization

--- hackathon/react/src/firebase.ts
+++ hackathon/react/src/firebase.ts
@@ -8,12 +8,12 @@ const firebaseConfig = {
 //   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
 //   messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
 //   appId: process.env.REACT_APP_APP_ID,
-  apiKey: AIzaSyBXgb7VEU9e4tE1qFDLRpuYY-7nqK5omuM,
-  authDomain: term3-mahiro-suematsu.firebaseapp.com,
-  projectId: term3-mahiro-suematsu,
-  storageBucket: term3-mahiro-suematsu.appspot.com,
-  messagingSenderId: 309741794676,
-  appId: c85c29add1d0f5c9092821,
+  apiKey: "AIzaSyBXgb7VEU9e4tE1qFDLRpuYY-7nqK5omuM",
+  authDomain: "term3-mahiro-suematsu.firebaseapp.com",
+  projectId: "term3-mahiro-suematsu",
+  storageBucket: "term3-mahiro-suematsu.appspot.com",
+  messagingSenderId: "309741794676",
+  appId: "c85c29add1d0f5c9092821",
 };
 
 // Initialize Firebase
