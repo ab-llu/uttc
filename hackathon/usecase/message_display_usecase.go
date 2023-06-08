c459e7c3248a721e6d0c0f69bdb033d803763307
mahiro <suematsu-mahiro863@g.ecc.u-tokyo.ac.jp>
2023-06-09 01:20:32 +0900

    Add errorlog

--- hackathon/usecase/message_display_usecase.go
+++ hackathon/usecase/message_display_usecase.go
@@ -12,8 +12,10 @@ func MessageDisplay(w http.ResponseWriter, r *http.Request, db *sql.DB) {
 	channel := r.FormValue("channel")
 
 	var messages = dao.MessageDisplay(w, db, channel)
+	log.Println("messages:", messages)
 
 	bytes, err := json.Marshal(messages)
+	log.Println("bytes:", bytes)
 	if err != nil {
 		log.Printf("fail: json.Marshal, %v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
