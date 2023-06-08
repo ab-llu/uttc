7a0d53d67a8817b86aecad9d83f41751fee602cb
mahiro <suematsu-mahiro863@g.ecc.u-tokyo.ac.jp>
2023-06-09 02:02:38 +0900

    Edit

--- hackathon/dao/user_dao.go
+++ hackathon/dao/user_dao.go
@@ -7,47 +7,17 @@ import (
 	"net/http"
 )
 
-func UserFetch(w http.ResponseWriter, db *sql.DB, userID string) []model.UserResForHTTPGet {
-	//row := db.QueryRow("SELECT userID, userNAME, email from user where userID = ?", userID)
-	//var userData model.UserResForHTTPGet
-	//log.Println("userData:", userData)
-	//if err := row.Scan(&userData.Id, &userData.Name, &userData.Email); err != nil {
-	//	log.Printf("fail: row.Scan %v\n", err)
-	//	w.WriteHeader(http.StatusInternalServerError)
-	//	return model.UserResForHTTPGet{}, nil
-	//}
-	//log.Println("userData:", userData)
-	//return userData, nil
-
-	rows, err := db.Query("SELECT * FROM user where userID = ?", userID)
-	if err != nil {
-		log.Printf("fail: db.Query, %v\n", err)
+func UserFetch(w http.ResponseWriter, db *sql.DB, userID string) (model.UserResForHTTPGet, error) {
+	row := db.QueryRow("SELECT userID, userNAME, email from user where userID = ?", userID)
+	var userData model.UserResForHTTPGet
+	log.Println("userData:", userData)
+	if err := row.Scan(&userData.Id, &userData.Name, &userData.Email); err != nil {
+		log.Printf("fail: row.Scan %v\n", err)
 		w.WriteHeader(http.StatusInternalServerError)
-		return nil
+		return model.UserResForHTTPGet{}, nil
 	}
-
-	users := make([]model.UserResForHTTPGet, 0)
-	for rows.Next() {
-		var userData model.UserResForHTTPGet
-		if err := rows.Scan(&userData.Id, &userData.Name, &userData.Email); err != nil {
-			log.Printf("fail: rows.Scan, %v\n", err)
-
-			if err := rows.Close(); err != nil { // 500を返して終了するが、その前にrowsのClose処理が必要
-				log.Printf("fail: rows.Close(), %v\n", err)
-			}
-			w.WriteHeader(http.StatusInternalServerError)
-			return nil
-		}
-
-		var user model.UserResForHTTPGet
-		user.Id = userData.Id
-		user.Name = userData.Name
-		user.Email = userData.Email
-
-		users = append(users, user)
-	}
-
-	return users
+	log.Println("userData:", userData)
+	return userData, nil
 }
 
 func UserRegister(user model.UserResForHTTPPOST, w http.ResponseWriter, db *sql.DB) {
